6cd889f5ffb050661df2faf693d88abfe85feedf
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-02-04 21:51:20 -0600

    without Stacks implementation

--- WeekTwo/StacksAssignment/Stacks.java
+++ WeekTwo/StacksAssignment/Stacks.java
@@ -15,11 +15,32 @@ import java.util.Stack;
  */
 
 public class Stacks {
-    public static boolean isPalindrome(String winput) {
 
+    /**
+     *  check if the input string by a user is a palindrome (a word or sentence that is spelled the same in reverse as it is spelled
+        straightforward). This method should work irrespective of spaces and casing. Example: the method should return true for “Race Car” and “racecar”. 
+     * @param winput - the input string.
+     * @return isPalindrome - true if the input string is a palindrome, false otherwise.
+     */
+    public static boolean isPalindrome(String winput) {
+        
+        //Stack<String> checkPali = new Stack<String>();
+        //boolean isPalindrome = checkPali.isEmpty();
+
+        boolean isPalindrome = false;
+        
+        winput = winput.toLowerCase().replaceAll(" ", "");
+        String reverse = "";
+
+        for(int i = winput.length() - 1; i >= 0; i--){
+            reverse += winput.charAt(i);
+        }
+            if(winput.equals(reverse)){
+                isPalindrome = true;
+            }   
         // YOR CODE CODE HERE, *USE* A STACK IMPLEMENTATION
   
-        return false;
+        return isPalindrome;
     }
 
     public static int findLargestK(Stack<Integer> stack, int k) {
