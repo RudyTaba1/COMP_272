f9701f4bbe7cd4cc46b48725ec8bfa3fd09faff9
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-02-22 16:28:39 -0600

    Init SetUp for WeekFive HW

---
+++ WeekFive/HashingExcersieAssignment/HashingExercise.java
@@ -0,0 +1,39 @@
+package WeekFive.HashingExcersieAssignment;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+class HashingExercise {
+
+  public double getAverage(HashMap<Integer, Integer> map,
+                           int[] array)
+  {
+      /*
+       *  YOUR CODE GOES HERE
+       *
+       *  DO NOT FORGET TO PLACE YOUR NAME ABOVE, ALSO
+       *  RECALL IF NO VALUES FOUND, RETURNING 0.0 IS NOT
+       *  CORRECT. MAKE SURE YOU ARE RETURNING 0.0/0.0
+       *  (WHICH RETURNS NOT A NUMBER, or 'NaN').
+       */
+
+      return 0.0 / 0;
+
+
+  }
+
+
+  public ArrayList<String> odd(HashMap<Integer, String> map) {
+
+      ArrayList<String> result = new ArrayList<>();
+
+      /*
+       *  YOUR CODE GOES HERE
+       */
+
+      return result;
+  }
+
+
+}
+
