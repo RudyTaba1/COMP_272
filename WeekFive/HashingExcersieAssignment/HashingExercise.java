28b9f6449fe7fb079b274212a93d559ecd9f4548
Rudy T <120225242+RudyTaba1@users.noreply.github.com>
2024-02-29 16:14:37 -0600

    avg method written

--- WeekFive/HashingExcersieAssignment/HashingExercise.java
+++ WeekFive/HashingExcersieAssignment/HashingExercise.java
@@ -1,23 +1,47 @@
-package WeekFive.HashingExcersieAssignment;
+package HashingExcersieAssignment;
 
 import java.util.HashMap;
 import java.util.ArrayList;
 
 class HashingExercise {
 
-  public double getAverage(HashMap<Integer, Integer> map,
-                           int[] array)
-  {
-      /*
-       *  YOUR CODE GOES HERE
+        /*
+       *  
        *
        *  DO NOT FORGET TO PLACE YOUR NAME ABOVE, ALSO
        *  RECALL IF NO VALUES FOUND, RETURNING 0.0 IS NOT
        *  CORRECT. MAKE SURE YOU ARE RETURNING 0.0/0.0
        *  (WHICH RETURNS NOT A NUMBER, or 'NaN').
        */
+  public double getAverage(HashMap<Integer, Integer> map,
+                           int[] array)
+  {
+    double NaN = 0.0/0;
+    double sum = 0;
+    double avg = sum/array.length;
+    
+    for(int i = 0; i<array.length;i++){
+      map.put(i+1, array[i]);
+    }
 
-      return 0.0 / 0;
+    
+    if(array.length == 0 || map.size() == 0){
+      return NaN;
+      }
+      else{
+        for(int i = 0; i < array.length; i++){
+          if(map.containsKey(array[i])){
+            sum += map.get(array[i]);
+          }
+        }
+        if(sum == 0){
+          return NaN;
+        }
+        else{
+          return avg;
+        }
+      
+      }
 
 
   }
